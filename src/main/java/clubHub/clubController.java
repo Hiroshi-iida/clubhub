0e18fb7c77569124b6a4ead8371e08cfcd8b055e
Hiroshi-iida <iichan.hiro@gmail.com>
2020-07-02 12:49:48 +0900

    メール機能サンプル

--- src/main/java/clubHub/clubController.java
+++ src/main/java/clubHub/clubController.java
@@ -31,6 +31,22 @@ import java.io.ByteArrayOutputStream;
 import org.apache.commons.codec.binary.Base64;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
+import javax.mail.*;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Controller
 @SpringBootApplication
@@ -48,7 +64,83 @@ public class clubController {
 	@Autowired
 	PhotoDataRepository photorepository;
 
+	
+	@Component
+	public class JavaMailSample {
+//	  private static final Logger log = LoggerFactory.getLogger(JavaMailSample.class);
 
+	  public void send(String subject, String content) {
+
+	    final String to = "iichan.hiro@gmail.com";
+	    final String from = "clubhub.h@gmail.com";
+
+	    // Google account mail address
+	    final String username = "clubhub.h@gmail.com";
+	    // Google App password
+	    final String password = "vmzebqndbtejjplb";
+
+	    //final String charset = "ISO-2022-JP";
+	    final String charset = "UTF-8";
+
+	    final String encoding = "base64";
+
+	    // for gmail
+	    String host = "smtp.gmail.com";
+	    String port = "587";
+	    String starttls = "true";
+
+	    // for local
+	    //String host = "localhost";
+	    //String port = "2525";
+	    //String starttls = "false";
+
+	    Properties props = new Properties();
+	    props.put("mail.smtp.host", host);
+	    props.put("mail.smtp.port", port);
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", starttls);
+
+	    props.put("mail.smtp.connectiontimeout", "10000");
+	    props.put("mail.smtp.timeout", "10000");
+
+	    props.put("mail.debug", "true");
+
+	    Session session = Session.getInstance(props,
+	    new javax.mail.Authenticator() {
+	       protected PasswordAuthentication getPasswordAuthentication() {
+	          return new PasswordAuthentication(username, password);
+	       }
+	    });
+
+	    try {
+	      MimeMessage message = new MimeMessage(session);
+
+	      // Set From:
+	      message.setFrom(new InternetAddress(from, "clubHub"));
+	      // Set ReplyTo:
+	      message.setReplyTo(new Address[]{new InternetAddress(from)});
+	      // Set To:
+	      message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+
+	      message.setSubject(subject, charset);
+	      message.setText(content, charset);
+
+	      message.setHeader("Content-Transfer-Encoding", encoding);
+
+	      Transport.send(message);
+
+	    } catch (MessagingException e) {
+	      throw new RuntimeException(e);
+	    } catch (UnsupportedEncodingException e) {
+	      throw new RuntimeException(e);
+	    }
+
+	  }
+
+	}
+	
+	
+	
 	public ModelAndView required(ModelAndView mav) {
 		mav.addObject("AccountName", session.getAttribute("sessionAccountName"));
 		mav.addObject("sdata", session.getAttribute("sessionSdata"));
@@ -126,6 +218,18 @@ public class clubController {
 		}
 
 	}
+	
+	
+	@RequestMapping("/mail")
+	public ModelAndView mail(ModelAndView mav) {
+		mav.setViewName("result");
+		mav.addObject("msg","メールを送信しました！！");
+	    JavaMailSample mailSend = new JavaMailSample();
+	    mailSend.send("JavaMail テストメール", "テストメールの本文");
+	    return mav;
+	}
+	
+	
 	
 	@RequestMapping(value = "/demo", method = RequestMethod.GET)
 	public ModelAndView demo(ModelAndView mav) {
@@ -903,5 +1007,5 @@ public class clubController {
 //		ph1.setId(1);
 		photorepository.saveAndFlush(ph1);
 	}
-
+	
 }
