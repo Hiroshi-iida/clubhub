5178196fe43bb9f138c31457c5a544ba1588293a
Hiroshi-iida <iichan.hiro@gmail.com>
2020-07-03 20:06:02 +0900

    検索機能改善（運動部or文化部だけで検索可能に

--- src/main/java/clubHub/repositories/PostDataRepository.java
+++ src/main/java/clubHub/repositories/PostDataRepository.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostDataRepository extends JpaRepository<PostData, Long>{
-	public Optional<PostData> findById(int name);
+	public List <PostData> findById(int id);
 	public List <PostData> findByType(String type);
+	public List <PostData> findByCategory(String category);
+	public List <PostData> findByAreaAndCategory(String area, String category);
 	public List <PostData> findByArea(String area);
 	public List <PostData> findByAreaAndType(String area, String type);
 	
